ab26049b1ec0fd5b35a41ddf3d1acd61b6ff907f
Marina Baskova <marinabaskova24@gmail.com>
2019-07-09 17:43:35 -0700

    added answers for python I

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,25 @@
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if (num % 2 == 0):
+        return True
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
 
+def is_even_odd(num):
+    if (is_even(num)):
+        print("Even!")
+    else:
+        print("Odd")
+
+
+# YOUR CODE HERE
+is_even_odd(num)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+result = f.read()
+print(result)
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open('bar.txt', 'w')
+b.write("Hello.\nThis is a test.\nLambda\n ")
+b.close()
+
+b = open('bar.txt', 'r')
+result = b.read()
+print(result)
+b.close()
